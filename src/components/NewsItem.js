dfa20f5dad644a257ceab1bbb7a5bacfae31b0ee
Mukeshmp <mukeshpaul5555@gmail.com>
2024-04-16 23:18:25 +0530

    Newsapp

---
+++ src/components/NewsItem.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+
+export class NewsItem extends Component {
+    render() {
+        let { title, description, imageUrl, newsUrl, author, date, source } = this.props;
+        return (
+            <div className="my-3" style={{ borderColor: "balck", borderStyle: "solid" }}>
+                <div className="card" >
+                    {/* <div style={{
+                        display: 'flex',
+                        justifyContent: 'flex-end',
+                        position: 'absolute',
+                        right: '0'
+                    }
+                    }>
+
+                        <span className="badge rounded-pill bg-danger"> {source} </span>
+                    </div> */}
+                    <span className="position-absolute top-0 start-50 translate-middle badge rounded-pill bg-danger">
+                        {source}
+                    </span>
+                    <img src={!imageUrl ? "https://fdn.gsmarena.com/imgroot/news/21/08/xiaomi-smart-home-india-annoucnements/-476x249w4/gsmarena_00.jpg" : imageUrl} className="card-img-top" alt="..." />
+                    <div className="card-body" style={{ backgroundColor: "#c4ecf4" }}>
+                        <h5 className="card-title">{title}  </h5>
+                        <p className="card-text">{description}</p>
+                        <p className="card-text"><small className="text-muted">By {!author ? "Unknown" : author} on  {new Date(date).toGMTString()}</small></p>
+                        <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-warning">Read More</a>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default NewsItem
