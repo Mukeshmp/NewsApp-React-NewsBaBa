4ac48eb69691d1fbc5a3b2538d84182e64002ea4
Mukeshmp <mukeshpaul5555@gmail.com>
2024-04-17 10:48:24 +0530

    added sticky bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,7 +7,7 @@ export class NavBar extends Component {
     render() {
         return (
             <div>
-                <nav className="navbar navbar-expand-lg navbar-success bg-warning">
+                <nav className="navbar fixed-top navbar-expand-lg navbar-success bg-warning">
                     <div className="container-fluid">
                         <Link className="navbar-brand" to="/" style={{ color: "red" }}><b>NewsBaBa</b></Link>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
